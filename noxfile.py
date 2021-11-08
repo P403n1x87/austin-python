11acfdfb9a011aac018692b4340e0277722c4d1b
Gabriele N. Tornetta <phoenix1987@gmail.com>
2021-11-08 21:26:04 +0000

    ci: add Python 3.10 runs

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ nox.options.sessions = "lint", "tests"
 # ---- Configuration ----
 
 
-SUPPORTED_PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
+SUPPORTED_PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9", "3.10"]
 
 PYTEST_OPTIONS = ["-vvvs", "--cov=austin", "--cov-report", "term-missing"]
 
